40ae9c3c56be233020b5ae9c36cbe6aa64c66b80
kanghaeun <xgkg0330@jnu.ac.kr>
2025-05-04 19:52:35 +0900

    feat: 북마크 여부 확인 함수 추가

--- src/components/bookmark.js
+++ src/components/bookmark.js
@@ -1,5 +1,9 @@
 let bookmarks = JSON.parse(localStorage.getItem("movieBookmarks")) || [];
 
+export function isBookmarked(movieId) {
+  return bookmarks.some((item) => item.id === movieId);
+}
+
 function saveBookmarks() {
   localStorage.setItem("movieBookmarks", JSON.stringify(bookmarks));
 }
